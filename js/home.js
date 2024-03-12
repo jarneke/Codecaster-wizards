64a7be8872f4b1c6e07d34db0fe89045edb33b22
Jarne Eldrup <s143700@ap.be>
2024-03-12 11:48:42 +0000

    info modal popup done

--- js/home.js
+++ js/home.js
@@ -1,2 +1,9 @@
-const myModal = new bootstrap.Modal(document.getElementById('infoModal'), options)
-myModal.modal("show")
+document.addEventListener("DOMContentLoaded", () => {
+    const myModal = new bootstrap.Modal(document.getElementById('infoModal'));
+    myModal.show();
+});
+let infoBtn = document.getElementById("infoBtn")
+infoBtn.addEventListener("click", () => {
+    const myModal = new bootstrap.Modal(document.getElementById('infoModal'));
+    myModal.show();
+})

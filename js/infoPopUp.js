286e822a0319ce7a8e008a9da0e1c61a2ea4d953
Jarne Eldrup <s143700@ap.be>
2024-03-19 19:31:04 +0000

    remember me functionaliteit toegevoegd

--- js/infoPopUp.js
+++ js/infoPopUp.js
@@ -1,12 +1,10 @@
 const showModal = localStorage.getItem("showModal");
 const closeInfo = document.getElementById("CloseInfo");
 const dontShowAgain = document.getElementById("dontShowAgain");
-
 if (showModal === "true" || showModal === null) {
     const myModal = new bootstrap.Modal(document.getElementById('infoModal'));
     myModal.show();
 }
-
 closeInfo.addEventListener("click", e => {
     e.preventDefault();
 

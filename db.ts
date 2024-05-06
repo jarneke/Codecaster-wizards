0e748aa422c23df9d375eea66fcd3c419c25eea8
Thomas Van Tiggelen <s148957@ap.be>
2024-05-06 09:58:30 +0000

    toevoeging

--- db.ts
+++ db.ts
@@ -1,160 +1,194 @@
 import { Db, Collection, MongoClient } from "mongodb";
-import * as i from "./interfaces"
-import * as f from "./functions"
+import * as i from "./interfaces";
+import * as f from "./functions";
 import Magic = require("mtgsdk-ts");
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
 // all devTips
 const mtgTips: i.Tip[] = [
-    { tip: "Let op je mana curve - zorg voor een goede verdeling van goedkope, mid-range en dure spreuken." },
-    { tip: "Vergeet je landdrops niet - het spelen van een land per beurt is cruciaal om aan je manavereisten te voldoen." },
-    { tip: "Ken de stack - begrijp hoe de stack werkt en de implicaties van het spelen van spreuken en vaardigheden op verschillende momenten." },
-    { tip: "Lees de kaarten zorgvuldig door - soms kan de bewoording een groot verschil maken in hoe een kaart functioneert." },
-    { tip: "Houd de levens totalen bij - zowel die van jou als van je tegenstander. Het is gemakkelijk te vergeten, maar het kan cruciaal zijn voor het plannen van je strategie." },
-    { tip: "Plan je beurten vooruit - denk na over je plays tijdens de beurt van je tegenstander om de efficiëntie te maximaliseren." },
-    { tip: "Breid niet te ver uit - wees voorzichtig met het inzetten van te veel middelen op het bord in één keer, want dit kan je kwetsbaar maken voor board wipes." },
-    { tip: "Weet wanneer je moet aanvallen en wanneer je je moet terugtrekken - soms is het beter om te wachten op een betere gelegenheid om aan te vallen dan je te haasten." },
-    { tip: "Sideboard effectief - heb een plan om met veelvoorkomende matchups om te gaan en pas je deck dienovereenkomstig aan tussen games." },
-    { tip: "Oefening, oefening, oefening - hoe meer je speelt, hoe beter je de complexiteit van het spel leert begrijpen en je vaardigheden verbetert." },
-    { tip: "Raak niet ontmoedigd door nederlagen - leren van je fouten is een belangrijk onderdeel om een betere speler te worden." },
-    { tip: "Veel plezier! - Magic is een spel, dus zorg ervoor dat je je amuseert en de ervaring waardeert, winnen of verliezen." },
+  {
+    tip: "Let op je mana curve - zorg voor een goede verdeling van goedkope, mid-range en dure spreuken.",
+  },
+  {
+    tip: "Vergeet je landdrops niet - het spelen van een land per beurt is cruciaal om aan je manavereisten te voldoen.",
+  },
+  {
+    tip: "Ken de stack - begrijp hoe de stack werkt en de implicaties van het spelen van spreuken en vaardigheden op verschillende momenten.",
+  },
+  {
+    tip: "Lees de kaarten zorgvuldig door - soms kan de bewoording een groot verschil maken in hoe een kaart functioneert.",
+  },
+  {
+    tip: "Houd de levens totalen bij - zowel die van jou als van je tegenstander. Het is gemakkelijk te vergeten, maar het kan cruciaal zijn voor het plannen van je strategie.",
+  },
+  {
+    tip: "Plan je beurten vooruit - denk na over je plays tijdens de beurt van je tegenstander om de efficiëntie te maximaliseren.",
+  },
+  {
+    tip: "Breid niet te ver uit - wees voorzichtig met het inzetten van te veel middelen op het bord in één keer, want dit kan je kwetsbaar maken voor board wipes.",
+  },
+  {
+    tip: "Weet wanneer je moet aanvallen en wanneer je je moet terugtrekken - soms is het beter om te wachten op een betere gelegenheid om aan te vallen dan je te haasten.",
+  },
+  {
+    tip: "Sideboard effectief - heb een plan om met veelvoorkomende matchups om te gaan en pas je deck dienovereenkomstig aan tussen games.",
+  },
+  {
+    tip: "Oefening, oefening, oefening - hoe meer je speelt, hoe beter je de complexiteit van het spel leert begrijpen en je vaardigheden verbetert.",
+  },
+  {
+    tip: "Raak niet ontmoedigd door nederlagen - leren van je fouten is een belangrijk onderdeel om een betere speler te worden.",
+  },
+  {
+    tip: "Veel plezier! - Magic is een spel, dus zorg ervoor dat je je amuseert en de ervaring waardeert, winnen of verliezen.",
+  },
 ];
 
 // get uri from enviroment variables
-const uri: any = process.env.MONGO_URI || "mongodb://localhost:27017"
+const uri: any = process.env.MONGO_URI || "mongodb://localhost:27017";
 // initialize Mongoclient
 export const client = new MongoClient(uri);
 // initialize database
 const db: Db = client.db("Codecaster");
 // exit function
 async function exit() {
-    try {
-        // try to close connection
-        await client.close();
-        console.log("[ - SERVER - ]=> Disconnected from database");
-    } catch (error) {
-        // if errors, log it
-        console.error(error);
-    }
-    process.exit(0);
+  try {
+    // try to close connection
+    await client.close();
+    console.log("[ - SERVER - ]=> Disconnected from database");
+  } catch (error) {
+    // if errors, log it
+    console.error(error);
+  }
+  process.exit(0);
 }
 /**
  * A function to connect to the database
  */
 export async function connect() {
-    // try to connect
-    try {
-        await client.connect();
-        console.log("[ - SERVER - ]=> Connected to database");
-        await seed();
-        // if application is exited, close connection
-        process.on("SIGINT", exit); // For manually stopping the server
-        process.on("SIGUSR2", exit); // For nodemon restarting on save
-    }
-    // else log error 
-    catch (error) {
-        console.error(error);
-    }
+  // try to connect
+  try {
+    await client.connect();
+    console.log("[ - SERVER - ]=> Connected to database");
+    await seed();
+    // if application is exited, close connection
+    process.on("SIGINT", exit); // For manually stopping the server
+    process.on("SIGUSR2", exit); // For nodemon restarting on save
+  } catch (error) {
+    // else log error
+    console.error(error);
+  }
 }
 /**
  * A function to seed the database if needed
  */
 async function seed() {
-    // initialize allCards
-    const allCards: Magic.Card[] = [];
-    const allUsers: i.User[] = [
-        {
-            firstName: "John",
-            lastName: "Doe",
-            userName: "John_Doe",
-            email: "John.Doe@mail.com",
-            description: "John Doe is a dynamic individual with a diverse skill set and a passion for excellence. With a background in [industry/field], he brings a unique blend of [skills/traits] to every project he undertakes. Whether he's [activity/task], [activity/task], or [activity/task], John approaches each endeavor with dedication and creativity. His ability to [skill/quality] and [skill/quality] make him a valuable asset to any team. Outside of work, John enjoys [hobbies/interests], [hobbies/interests], and [hobbies/interests]. With a commitment to continuous growth and a drive to succeed, John is poised to make a significant impact in [industry/field].",
-            password: "Passw0rd123"
-          },
-          {
-            firstName: "Jane",
-            lastName: "Smith",
-            userName: "Smiley_Jane",
-            email: "Jane.Smith@mail.com",
-            description: "Jane Smith is a highly motivated individual with a strong work ethic and a passion for [industry/field]. With a background in [industry/field], she brings extensive experience and a proven track record of success to every project she undertakes. Whether she's [activity/task], [activity/task], or [activity/task], Jane consistently delivers high-quality results with precision and efficiency. Her exceptional [skills/traits] and [skills/traits] make her a valuable asset to any team. Outside of work, Jane enjoys [hobbies/interests], [hobbies/interests], and [hobbies/interests]. With a dedication to continuous improvement and a focus on achieving her goals, Jane is well-positioned to excel in [industry/field].",
-            password: "Jane1234"
-          },
-          {
-            firstName: "Michael",
-            lastName: "Johnson",
-            userName: "Mighty_Mike",
-            email: "Michael.Johnson@mail.com",
-            description: "Michael Johnson is a results-oriented professional with a proven track record of success in [industry/field]. With a background in [industry/field], he brings a wealth of knowledge and expertise to every project he undertakes. Whether he's [activity/task], [activity/task], or [activity/task], Michael consistently exceeds expectations and delivers exceptional results. His strong [skills/traits] and [skills/traits] enable him to thrive in fast-paced environments and tackle challenges with confidence. Outside of work, Michael enjoys [hobbies/interests], [hobbies/interests], and [hobbies/interests]. With a commitment to excellence and a drive to succeed, Michael is well-equipped to make a significant impact in [industry/field].",
-            password: "Michael!23"
-          }
-    ];
-    // variable to store amount of loadedcards
-    let loadedCardCount = 0;
-    // The amount of cards we want to load
-    const desiredCardCount = 100; // Change this to the desired number of cards to load before generating mock decks
+  // initialize allCards
+  const allCards: Magic.Card[] = [];
+  const allUsers: i.User[] = [
+    {
+      firstName: "John",
+      lastName: "Doe",
+      userName: "John_Doe",
+      email: "John.Doe@mail.com",
+      description:
+        "John Doe is a dynamic individual with a diverse skill set and a passion for excellence. With a background in [industry/field], he brings a unique blend of [skills/traits] to every project he undertakes. Whether he's [activity/task], [activity/task], or [activity/task], John approaches each endeavor with dedication and creativity. His ability to [skill/quality] and [skill/quality] make him a valuable asset to any team. Outside of work, John enjoys [hobbies/interests], [hobbies/interests], and [hobbies/interests]. With a commitment to continuous growth and a drive to succeed, John is poised to make a significant impact in [industry/field].",
+      password: "Passw0rd123",
+      role: "ADMIN",
+    },
+    {
+      firstName: "Jane",
+      lastName: "Smith",
+      userName: "Smiley_Jane",
+      email: "Jane.Smith@mail.com",
+      description:
+        "Jane Smith is a highly motivated individual with a strong work ethic and a passion for [industry/field]. With a background in [industry/field], she brings extensive experience and a proven track record of success to every project she undertakes. Whether she's [activity/task], [activity/task], or [activity/task], Jane consistently delivers high-quality results with precision and efficiency. Her exceptional [skills/traits] and [skills/traits] make her a valuable asset to any team. Outside of work, Jane enjoys [hobbies/interests], [hobbies/interests], and [hobbies/interests]. With a dedication to continuous improvement and a focus on achieving her goals, Jane is well-positioned to excel in [industry/field].",
+      password: "Jane1234",
+      role: "USER",
+    },
+    {
+      firstName: "Michael",
+      lastName: "Johnson",
+      userName: "Mighty_Mike",
+      email: "Michael.Johnson@mail.com",
+      description:
+        "Michael Johnson is a results-oriented professional with a proven track record of success in [industry/field]. With a background in [industry/field], he brings a wealth of knowledge and expertise to every project he undertakes. Whether he's [activity/task], [activity/task], or [activity/task], Michael consistently exceeds expectations and delivers exceptional results. His strong [skills/traits] and [skills/traits] enable him to thrive in fast-paced environments and tackle challenges with confidence. Outside of work, Michael enjoys [hobbies/interests], [hobbies/interests], and [hobbies/interests]. With a commitment to excellence and a drive to succeed, Michael is well-equipped to make a significant impact in [industry/field].",
+      password: "Michael!23",
+      role: "USER",
+    },
+  ];
+  // variable to store amount of loadedcards
+  let loadedCardCount = 0;
+  // The amount of cards we want to load
+  const desiredCardCount = 100; // Change this to the desired number of cards to load before generating mock decks
 
-    // initialize emitter to get cards
-    const emitter = await Magic.Cards.all({})
-        // on card recieved
-        .on("data", async (card) => {
-            // check if the card has a imageUrl
-            if (card.imageUrl !== undefined) {
-                // push card to allCards array
-                allCards.push(card);
-                // and update counter
-                loadedCardCount++;
+  // initialize emitter to get cards
+  const emitter = await Magic.Cards.all({})
+    // on card recieved
+    .on("data", async (card) => {
+      // check if the card has a imageUrl
+      if (card.imageUrl !== undefined) {
+        // push card to allCards array
+        allCards.push(card);
+        // and update counter
+        loadedCardCount++;
 
-                // Check if desired number of cards have been loaded
-                if (loadedCardCount >= desiredCardCount) {
-                    // cancel the emmit to stop getting cards
-                    emitter.cancel();
-                    // generate mockDecks
-                    const mockDecks: i.Deck[] = await f.generateMockDecks(allCards);
-                    // populate the database if need be with mockDecks
-                    await populateDatabase(mockDecks);
-                    // populate the database if need be with tips
-                    await populateTips(mtgTips);
-                    // log that the db is seeded
-                    await console.log("[ - SERVER - ]=> Done seeding the database");
-                }
-            }
-        })
-        // if error occurs with loading cards, log it
-        .on("error", (e) => console.error("ERROR: " + e));
+        // Check if desired number of cards have been loaded
+        if (loadedCardCount >= desiredCardCount) {
+          // cancel the emmit to stop getting cards
+          emitter.cancel();
+          // generate mockDecks
+          const mockDecks: i.Deck[] = await f.generateMockDecks(allCards);
+          // populate the database if need be with mockDecks
+          await populateDatabase(mockDecks);
+          // populate the database if need be with tips
+          await populateTips(mtgTips);
+          // log that the db is seeded
+          await console.log("[ - SERVER - ]=> Done seeding the database");
+        }
+      }
+    })
+    // if error occurs with loading cards, log it
+    .on("error", (e) => console.error("ERROR: " + e));
 }
 /**
  * a function to insert mock decks into database if needed
  * @param mockDecks array of mockDecks
  */
 async function populateDatabase(mockDecks: i.Deck[]) {
-    // uncomment line beneath if you want to refresh decks in database
-    // decksCollection.deleteMany({});
+  // uncomment line beneath if you want to refresh decks in database
+  // decksCollection.deleteMany({});
 
-    // if decksCollection is empty insert and log that its added
-    if (await decksCollection.countDocuments() === 0) {
-        await decksCollection.insertMany(mockDecks);
-        console.log("[ - SERVER - ]=> Mock decks inserted into database");
-    }
+  // if decksCollection is empty insert and log that its added
+  if ((await decksCollection.countDocuments()) === 0) {
+    await decksCollection.insertMany(mockDecks);
+    console.log("[ - SERVER - ]=> Mock decks inserted into database");
+  }
 }
 
 /**
  * A function to populate the database with tips if need be
  */
 export async function populateTips(allTips: i.Tip[]) {
-    // uncomment line beneath if you want to refresh tips in database
-    //await tipsCollection.deleteMany({});
+  // uncomment line beneath if you want to refresh tips in database
+  //await tipsCollection.deleteMany({});
 
-    // if decksCollection is empty insert and log that its added
-    if (await tipsCollection.countDocuments() === 0) {
-        await tipsCollection.insertMany(allTips);
-    }
+  // if decksCollection is empty insert and log that its added
+  if ((await tipsCollection.countDocuments()) === 0) {
+    await tipsCollection.insertMany(allTips);
+  }
 }
 
+// initialize usersCollection and export it to be used outside of db setup
+export const usersCollection: Collection<i.User> =
+  db.collection<i.User>("Users");
 // initialize decksCollection and export it to be used outside of db setup
-export const decksCollection: Collection<i.Deck> = db.collection<i.Deck>("Decks");
+export const decksCollection: Collection<i.Deck> =
+  db.collection<i.Deck>("Decks");
 // initialize feedbacksCollection and export it to be used outside of db setup
-export const feedbacksCollection: Collection<i.Feedback> = db.collection<i.Feedback>("Feedbacks");
+export const feedbacksCollection: Collection<i.Feedback> =
+  db.collection<i.Feedback>("Feedbacks");
 // initialize tipsCollection and export it to be used outside of db setup
 export const tipsCollection: Collection<i.Tip> = db.collection<i.Tip>("Tips");
--- interfaces.ts
+++ interfaces.ts
@@ -1,55 +1,48 @@
 import Magic = require("mtgsdk-ts");
-import * as f from "./functions"
+import * as f from "./functions";
 import { ObjectId } from "mongodb";
 
 // interface for user
 export interface User {
-    firstName: string,
-    lastName: string
-    userName: string,
-    email: string,
-    description: string,
-    password: string,
-    _id?: ObjectId
-}
-export const tempUser: User = {
-    firstName: "John",
-    lastName: "",
-    userName: "",
-    email: "",
-    description: "",
-    Password: ""
+  firstName: string;
+  lastName: string;
+  userName: string;
+  email: string;
+  description: string;
+  password?: string;
+  role: "ADMIN" | "USER";
+  _id?: ObjectId;
 }
 // interface for pageData
 export interface PageData {
-    page: number,
-    totalPages: number,
-    filterUrl: string
+  page: number;
+  totalPages: number;
+  filterUrl: string;
 }
 // enum for feedbackType
 export enum feedbackType {
-    bug,
-    suggestion,
-    compliment,
-    general
+  bug,
+  suggestion,
+  compliment,
+  general,
 }
 // interface for feedback
 export interface Feedback {
-    feedbackType: feedbackType,
-    feedback: string,
+  feedbackType: feedbackType;
+  feedback: string;
 }
 // extention of Magic.Card interface, to allow dinamic calling of object params
 export interface Card extends Magic.Card {
-    [key: string]: any,
+  [key: string]: any;
 }
 // interface for decks
 export interface Deck {
-    deckName: string,
-    cards: Magic.Card[],
-    deckImageUrl: string
+  deckName: string;
+  cards: Magic.Card[];
+  deckImageUrl: string;
 }
 // interface for tips
 export interface Tip {
-    _id?: ObjectId,
-    tip: string
+  _id?: ObjectId;
+  tip: string;
 }

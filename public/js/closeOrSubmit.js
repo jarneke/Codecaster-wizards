ae8c9adbe42cbff83bc455d2643dd22ca89c8694
jarneke <jarne.eldrup@outlook.com>
2024-05-21 19:45:41 +0000

    -

--- public/js/closeOrSubmit.js
+++ public/js/closeOrSubmit.js
@@ -4,11 +4,16 @@ document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("infoForm");
     const btn = document.getElementById("iUnderstand");
     const checkbox = document.getElementById("dontShowAgain");
+    const checkboxLabel = document.getElementById("dontShowAgainLabel")
 
     btn.addEventListener("click", () => {
         console.log("clicked");
         if (checkbox.checked) {
             form.submit();
+        } else {
+            for (let i = 0; i < 6; i++) {
+                checkboxLabel.classList.add("blink-anim")
+            }
         }
     });
 });
